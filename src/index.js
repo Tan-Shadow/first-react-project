b67184d6ab94fa67cfe7332eac7dc1cf87d4e216
Tan Shadow <kagekun018@gmail.com>
2021-08-21 20:54:21 +0530

    added winner msg and ignores the clicking if someone already won

--- src/index.js
+++ src/index.js
@@ -21,10 +21,19 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (!this.xIsNext) squares[i] = "x";
-    else squares[i] = "o";
-    this.xIsNext = !this.xIsNext;
-    this.setState({ squares: squares });
+
+    if (calculateWinner(squares)) {
+      return;
+    }
+
+    // if (this.xIsNext) squares[i] = "x";
+    // else squares[i] = "o";
+
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -37,7 +46,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner! -> ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
 
     return (
       <div>
@@ -76,6 +91,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
